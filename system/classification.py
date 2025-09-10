4980893bd42c05299826a53f01a417781694f5c1
shuobin <3120465015@qq.com>
2025-09-10 15:30:31 +0800

    add

--- system/classification.py
+++ system/classification.py
@@ -11,6 +11,7 @@ st.title("分类与地物识别")
 # 思路：直接调用你训练好的模型即可，无需重复训练。
 
 st.markdown("# 监督分类")
+st.markdown("## 随机森林模型")
 
 uploaded_img = st.file_uploader(
     "请上传你要分类的图像", accept_multiple_files=False, type=["tif", "tiff"]
@@ -70,11 +71,15 @@ if uploaded_img is not None:
         )
 
 
-
-
-
-
-
 st.markdown("# 非监督分类")
+st.markdown("## K均值模型")
+
+
+
+
+
+
+
+
 st.markdown("# 对象导向分类")
 st.markdown("# 深度学习")
